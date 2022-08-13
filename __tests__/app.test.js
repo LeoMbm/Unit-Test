7572a5a0c0e941022565086e2b5b1f543734d14c
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-14 00:28:22 +0200

    update

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,4 +3,7 @@ describe('Palindrome', () => {
     it('should return a palindrome', () => {
         expect(app.isPalindrome('kayak')).toEqual(true);
     })
+    it('should not return a palindrome', () => {
+        expect(app.isPalindrome('jeremy')).toEqual(false);
+    })
 })
